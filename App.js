a5569574dbae4a718e68b59966ba77d50d33f4be
Kevin R Salger <kevin.salger@student.csulb.edu>
2021-06-10 20:18:07 -0700

    Mid 1st

--- App.js
+++ App.js
@@ -1,30 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Platform, StyleSheet, Text, TextInput, View } from 'react-native';
 
 
 
 export default function App() {
-  const [number, onChangeNumber] = React.useState(0);
+  const [number1, onChangeNumber1] = React.useState(0);
+  const [number2, onChangeNumber2] = React.useState(0);
   const [message, setMessage] = React.useState("");
 
-  function getSquare() {
-    const squared = number * number;
+  function doCompare(){
+    // User numbers < 2 && > 100 are invalid
+    
+    if (number1 < 2) {
+      // Send error message 
+      ()=>setMessage(errorMessage());
+    } 
+    else if (number1 > 100) {
+      // Send error message
+      ()=>setMessage(errorMessage());
+    }
+    else if (number2 < 2) {
+      // Send error message
+      ()=>setMessage(errorMessage());
+    }
+    else if (number2 > 100) {
+      // Send error message
+      ()=>setMessage(errorMessage());
+    }
+    
+    if (number1 > number2) {
+      // Proclaim Number 1 is greater than Number 2
+      ()=>setMessage(greater());
+    }
+    else if (number1 === number2) {
+      ()=>setMessage(equaler());
+    }
+    else if (number1 < number2) {
+      ()=>setMessage(lesser());
+    } 
+    return(
+      <View>{message}</View>
+    )
+  }
+
+  function errorMessage(){
+    return <View color="red">Invalid input, please try again.</View>
+  }
+  function greater(){
+    return <View color="green">Number 1 is bigger than Number 2.</View>
+  }
+  function equaler(){
+    return <View color="green">Number 1 is equal to Number 2.</View>
+  }
+  function lesser(){
+    return <View color="green">Number 1 is less than Number 2.</View>
+  }
+/*
+  function getCompare() {
+    const greater = number1 > number2;
     return `The square of ${number} is ${squared}`;
   }
+*/
 
   return (
     <View style={styles.container}>
-      <Text>Input a number:</Text>
-
+      <View style={styles.box}>
+        <Text style={styles.boxTextHead}>Midterm Exam</Text>
+        <Text style={styles.boxTextBody}>Compare two numbers</Text>
+      </View>
+      
+      <Text style={styles.boxTextNum}>Number 1:</Text>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
+        onChangeText={onChangeNumber1}
+        value={number1}
         keyboardType="numeric"
         />
-        <Button title="Calculate the square" 
-                onPress={() => setMessage(getSquare())} />
+      <Text style={styles.boxTextNum}>Number 2:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber2}
+        value={number2}
+        keyboardType="numeric"
+        />
+
+
+        <Button title="COMPARE" 
+                color="#0088aa"
+                onPress={() => setMessage(doCompare())} />
         <StatusBar style="auto" />
         <Text>{message} </Text>
     </View>
@@ -36,8 +100,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    ...Platform.select ({
+       ios: {paddingTop: 20},
+       android: {paddingTop: StatusBar.currentHeight}
+    })
   },
+
+  box: {
+    //width: 450,
+    height: 250,
+
+    alignSelf: "stretch",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "center",
+    backgroundColor: "#0088aa"
+  },
+
+  boxTextHead: {
+    fontSize: 42,
+    color: "#ffffff",
+    //alignItems: "center"
+  },
+
+  boxTextBody: {
+    fontSize: 21,
+    color: "#ffffff"
+  },
+
+  boxTextNum: {
+    fontSize: 15,
+    color: "#000000",
+    alignSelf: "center"
+  }
 });
